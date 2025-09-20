e7d6b7fe7855490c1179c80e7d290d2e5f657080
Zlobryak <clychnik@gmail.com>
2025-09-20 23:24:43 +0300

    Fee calculations changed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
 package ru.netology
 
 fun main() {
-    val amount = 150_000.0
-    val cardType = "Mastercard"
-    val amountDay = 75_000.0 //Переводы за день
+    val amount = 1_000.0
+//    val amount = 15_000.0
+//    val amount = 75_000.0
+//    val cardType = "Mastercard"
+    val cardType = "Visa"
+//    val cardType = "Mir"
+    val amountDay = 65_000.0 //Переводы за день
     val amountMonth = 75_000.0 //Переводы за месяц
 
     moneyTransfer(amount, cardType, amountDay, amountMonth)
@@ -13,91 +17,50 @@ fun moneyTransfer(amount: Double, cardType: String, amountDay: Double = 0.0, amo
     //Значения для карт Visa
     val visaFee = 0.0075
     val visaMinFee = 35.0
-    val visaNoFeeLimit = 0.0
+//    val visaNoFeeLimit = 0.0 (не используется по условию задачи
     //Значения для карт Mastercard
     val mastercardFee = 0.006
     val mastercardMinFee = 20.0
     val mastercardNoFeeLimit = 75_000.0
-    //Значения для карт Mir
-    val mirFee = 0.0
-    val mirMinFee = 0.0
-    val mirNoFeeLimit = 0.0
+    //Значения для карт Mir (не использую, т.к. по условию задачи комиссия не взимается за переводы с карты мир
+//    val mirFee = 0.0
+//    val mirMinFee = 0.0
+//    val mirNoFeeLimit = 0.0
     //Дневной и месячный лимиты
     val dayLimit = 150_000.0
     val monthLimit = 600_000.0
     //Сначала проверим, не превышен ли один из лимитов
+
     when ((amount + amountDay) > dayLimit || amount + amountMonth > monthLimit) {
         true -> println("Превышен лимит переводов")
-        //Если лимиты позволяют, вызовем функцию считающую комиссию
+        //
         else -> when (cardType) {
-            "Mastercard" -> println(
-                "Переведено $amount руб. Комиссия составит ${
-                    String.format(
-                        "%.2f",
-                        feeCalculator(
-                            amount,
-                            mastercardFee,
-                            mastercardMinFee,
-                            mastercardNoFeeLimit,
-                            amountMonth
-                        )
-                    )
-                } руб."
+            "Mastercard" -> {
+                val monthAmountCalc = amountMonth + amount
+                val fee = when (monthAmountCalc < mastercardNoFeeLimit) {
+                    true -> 0.0
+                    false -> (monthAmountCalc - mastercardNoFeeLimit) * mastercardFee + mastercardMinFee
+                }
+                println("Переведено $amount руб. Комиссия составит ${String.format("%.2f", fee)} руб.")
+            }
 
-            )
-
-            "Visa" -> println(
-                "Переведено $amount  Комиссия составит ${
-                    String.format(
-                        "%.2f",
-                        feeCalculator(
-                            amount,
-                            visaFee,
-                            visaMinFee,
-                            visaNoFeeLimit,
-                            amountMonth
-                        )
-                    )
-                } руб."
-
-            )
+            "Visa" -> {
+                val fee = amount * visaFee
+                println(
+                    "Переведено $amount  Комиссия составит ${
+                        String.format("%.2f", maxOf(visaMinFee, fee))
+                    } руб."
+                )
+            }
 
             "Mir" -> println(
-                "Переведено $amount Комиссия составит ${
-                    String.format(
-                        "%.2f",
-                        feeCalculator(
-                            amount,
-                            mirFee,
-                            mirMinFee,
-                            mirNoFeeLimit,
-                            amountMonth
-                        )
-                    )
-                } руб ."
+                "Переведено $amount Комиссия составит 0 руб ."
 
             )
-
         }
     }
 }
 
-fun feeCalculator(
-    amount: Double, //Сумма перевода
-    fee: Double, //Процент комиссии
-    minFee: Double, //Минимальная комиссия
-    noFeeLimit: Double,
-    amountMonth: Double, //Месячный лимит на переводы
-): Double {
-    //Первым делом проверим не превышен ли месячный лимит на переводы
-    //и вычислим от какой суммы нужно вычислить комиссию
-    val overLimit = maxOf(0.0, amountMonth + amount - noFeeLimit)
-    return when (overLimit != 0.0) {
-        true -> overLimit * fee + minFee
-        else -> 0.0
-
-    }
-}
 
 /*
 За переводы с карты Mastercard комиссия не взимается,
